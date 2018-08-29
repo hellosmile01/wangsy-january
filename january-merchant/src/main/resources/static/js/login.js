44e1bb30e0c881f574ab5cd7c1ba19d4f174cd23
smilewangsy <smilewangsy@outlook.com>
2018-08-29 18:05:29 +0800

    no message

--- january-merchant/src/main/resources/static/js/login.js
+++ january-merchant/src/main/resources/static/js/login.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#login',
     data: {
+        activeIndex: '1',
         message: "You Have not Login.(Merchant)"
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
     }
 });
