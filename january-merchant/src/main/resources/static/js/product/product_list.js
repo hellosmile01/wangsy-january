de83f02fbea97646d9eec371e83ac2e6f41327ea
smilewang <smilewangsy@outlook.com>
2018-08-31 01:08:28 +0800

    no message

--- january-merchant/src/main/resources/static/js/product/product_list.js
+++ january-merchant/src/main/resources/static/js/product/product_list.js
@@ -2,6 +2,16 @@
 const $table = $('#product_list');
 
 $table.bootstrapTable({
+    url: "/v1/products",
+    method: "get",
+    sidePagination:"server",
+    queryParams: function(params){
+        var queryData = {};    //如果没有额外的查询参数的话就新建一个空对象，如果有的话就先装你的查询参数
+        //然后增加这两个
+        queryData.limit = params.limit;
+        queryData.offset = params.offset;
+        return queryData;    //这个就是向服务端传递的参数对象
+    },
     columns: [{
         field: 'id',
         title: 'ID'
