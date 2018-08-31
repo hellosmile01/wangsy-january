0d3c9421ae7bcffe3c73e30ea426702512149ce3
smilewangsy <smilewangsy@outlook.com>
2018-08-31 16:35:54 +0800

    no message

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # wangsy-january
+
+> `january-member:`用户会员服务
+>
+> `january-merchant:`商户服务
+>
+> `january-order:`订单服务
+>
---
+++ january-order/src/main/java/smile/wangsy/january/order/JanuaryOrderApplication.java
@@ -0,0 +1,12 @@
+package smile.wangsy.january.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanuaryOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanuaryOrderApplication.class, args);
+	}
+}
