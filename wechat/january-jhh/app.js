4232d4566ff84bd50b29ac5a00e399bc6e98cf11
smilewang <smilewangsy@outlook.com>
2018-09-01 00:26:53 +0800

    小程序

--- wechat/january-jhh/app.js
+++ wechat/january-jhh/app.js
@@ -10,11 +10,13 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res);
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        debugger;
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
