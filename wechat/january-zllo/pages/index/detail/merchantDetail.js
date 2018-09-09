72cd7a5a9e86379b5d645a726c01d113f18512e8
smilewang <smilewangsy@outlook.com>
2018-09-10 00:59:37 +0800

    修改菜单

--- wechat/january-zllo/pages/index/detail/merchantDetail.js
+++ wechat/january-zllo/pages/index/detail/merchantDetail.js
@@ -1,4 +1,13 @@
 // pages/index/detail/merchantDetail.js
+
+const constants = require('../../../utils/constants.js');
+
+const RIGHT_BAR_HEIGHT = 20;      // 右侧每一类的 bar 的高度（固定）
+const RIGHT_ITEM_HEIGHT = 60;     // 右侧每个子类的高度（固定）
+const LEFT_ITEM_HEIGHT = 50       // 左侧每个类的高度（固定）
+
+let windowHeight;
+
 Page({
 
     /**
@@ -6,15 +15,129 @@ Page({
      */
     data: {
 		imageUrl: '/images/logo0.jpg',
-		active: 0
+		tab_active: 0,
+		badge_active:0,
+		menu_active: 0,
+		menus: [
+			{
+				'name': '热销',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '折扣',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '招牌菜',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '江浙菜',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '川菜',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '湘菜',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '甜品',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '饮品',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '狗粮',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+			{
+				'name': '赠送',
+				'details': [
+					{
+						'name': 'd',
+						'price': 19
+					}
+				]
+			},
+		],
+		constants: [],                // 数据
+		toView: null,                 // 左 => 右联动 右scroll-into-view 所需的id
+		currentLeftSelect: null,      // 当前左侧选择的
+		eachRightItemToTop: [],       // 右侧每类数据到顶部的距离（用来与 右 => 左 联动时监听右侧滚动到顶部的距离比较）
+		leftToTop: 0,
+		leftHeight:0
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log(options);
-        console.log("merchantId"+options.merchantId);
+        
+		this.getSystemInfo();
+
+		let lh = windowHeight;
+
+debugger;
+		if ((constants.length - 1) * LEFT_ITEM_HEIGHT>lh) {
+			lh = (constants.length - 1) * LEFT_ITEM_HEIGHT;
+		}
+
+		this.setData({
+			constants: constants,
+			currentLeftSelect: constants[0].id,
+			eachRightItemToTop: this.getEachRightItemToTop(),
+			leftHeight: lh
+		})
 
         wx.setNavigationBarTitle({
             title: options.merchantName,
@@ -27,6 +150,17 @@ Page({
         });
     },
 	onChange: function(event) {
+		console.log(event);
+		this.setData({
+			tab_active: event.detail.index
+		});
+		wx.showToast({
+			icon: 'none',
+			title: `Tab切换至第${event.detail.index}项`
+		});
+	},
+
+	onBadgeChange: function (event) {
 		console.log(event);
 		wx.showToast({
 			icon: 'none',
@@ -81,5 +215,42 @@ Page({
      */
     onShareAppMessage: function () {
 
-    }
+    },
+	getEachRightItemToTop: function () {  // 获取每个右侧的 bar 到顶部的距离，用来做后面的计算。
+		var obj = {};
+		var totop = 0;
+		obj[constants[0].id] = totop      // 右侧第一类肯定是到顶部的距离为 0
+		for (let i = 1; i < (constants.length + 1); i++) {  // 循环来计算每个子类到顶部的高度
+			totop += (RIGHT_BAR_HEIGHT + constants[i - 1].category.length * RIGHT_ITEM_HEIGHT)
+			// 这个的目的是 例如有两类，最后需要 0-1 1-2 2-3 的数据，所以需要一个不存在的 'last' 项，此项即为第一类加上第二类的高度。
+			obj[constants[i] ? constants[i].id : 'last'] = totop    
+		}
+		return obj
+	},
+	rightScroll: function (e) {   // 监听右侧的滚动事件与 eachRightItemToTop 的循环作对比 从而判断当前可视区域为第几类，从而渲染左侧的对应类。
+		for (let i = 0; i < this.data.constants.length; i++) {
+			let left = this.data.eachRightItemToTop[this.data.constants[i].id]
+			let right = this.data.eachRightItemToTop[this.data.constants[i + 1] ? this.data.constants[i + 1].id : 'last']
+			if (e.detail.scrollTop < right && e.detail.scrollTop >= left) {
+				this.setData({
+					currentLeftSelect: this.data.constants[i].id,
+					leftToTop: LEFT_ITEM_HEIGHT * i
+				})
+			}
+		}
+	},
+	jumpTo: function (e) {    // 左侧类的点击事件，点击时，右侧会滚动到对应分类
+		this.setData({
+			toView: e.target.id || e.target.dataset.id,
+			currentLeftSelect: e.target.id || e.target.dataset.id
+		})
+	},
+	getSystemInfo() {
+		let self = this
+		wx.getSystemInfo({
+			success: function (res) {
+				windowHeight = res.windowHeight;
+			}
+		})
+	}
 })
---
+++ wechat/january-zllo/utils/constants.js
@@ -0,0 +1,227 @@
+const constants = [
+	{
+		"id": "id1",
+		"name": "热销",
+		"category": [
+			{
+				"category_id": 1,
+				"category_name": "绿茶烤鸡",
+			},
+			{
+				"category_id": 2,
+				"category_name": "香菜土豆丝",
+			},
+			{
+				"category_id": 3,
+				"category_name": "糖醋里脊",
+			},
+			{
+				"category_id": 4,
+				"category_name": "干锅花菜",
+			}
+		],
+	},
+	{
+		"id": "id2",
+		"name": "折扣",
+		"category": [
+			{
+				"category_id": 4,
+				"category_name": "石锅豆腐鸡汤",
+			},
+			{
+				"category_id": 5,
+				"category_name": "古法腊烧鸡",
+			},
+			{
+				"category_id": 6,
+				"category_name": "绿茶炒饭",
+			},
+			{
+				"category_id": 7,
+				"category_name": "面包诱惑",
+			},
+			{
+				"category_id": 8,
+				"category_name": "特惠A套餐",
+			}, {
+				"category_id": 9,
+				"category_name": "特惠B套餐",
+			},
+			{
+				"category_id": 10,
+				"category_name": "特惠C套餐",
+			}
+		]
+	},
+	{
+		"id": "id3",
+		"name": "招牌菜",
+		"category": [
+			{
+				"category_id": 17,
+				"category_name": "鱼头豆腐汤",
+			},
+			{
+				"category_id": 18,
+				"category_name": "鱼头诱惑",
+			},
+			{
+				"category_id": 19,
+				"category_name": "绿茶烤鸡",
+			},
+			{
+				"category_id": 19,
+				"category_name": "绿茶烤鱼",
+			},
+			{
+				"category_id": 19,
+				"category_name": "绿茶大虾",
+			}
+		],
+	},
+	{
+		"id": "id4",
+		"name": "家常菜",
+		"category": [
+			{
+				"category_id": 20,
+				"category_name": "粉丝娃娃菜",
+			},
+			{
+				"category_id": 21,
+				"category_name": "香菜土豆丝",
+			},
+			{
+				"category_id": 22,
+				"category_name": "干锅花菜",
+			},
+			{
+				"category_id": 23,
+				"category_name": "水煮肉片",
+			},
+			{
+				"category_id": 24,
+				"category_name": "小锅土豆",
+			},
+			{
+				"category_id": 25,
+				"category_name": "麻婆豆腐",
+			}
+		],
+	},
+	{
+		"id": "id5",
+		"name": "无肉不欢",
+		"category": [
+			{
+				"category_id": 33,
+				"category_name": "烤鸡翅",
+			},
+			{
+				"category_id": 34,
+				"category_name": "锅包肉",
+			},
+			{
+				"category_id": 35,
+				"category_name": "东坡肉",
+			},
+			{
+				"category_id": 35,
+				"category_name": "烤鸭",
+			},
+			{
+				"category_id": 35,
+				"category_name": "糖醋排骨",
+			}
+		],
+	},
+	{
+		"id": "id6",
+		"name": "健康时蔬",
+		"category": [
+			{
+				"category_id": 36,
+				"category_name": "空心菜",
+			},
+			{
+				"category_id": 37,
+				"category_name": "菜心煮豆腐",
+			},
+			{
+				"category_id": 38,
+				"category_name": "上汤西兰花",
+			}
+		],
+	},
+	{
+		"id": "id7",
+		"name": "开胃凉菜",
+		"category": [
+			{
+				"category_id": 39,
+				"category_name": "卤鸡爪",
+			},
+			{
+				"category_id": 40,
+				"category_name": "爽口木耳",
+			}
+		],
+	},
+	{
+		"id": "id8",
+		"name": "主食",
+		"category": [
+			{
+				"category_id": 42,
+				"category_name": "米饭",
+			},
+			{
+				"category_id": 43,
+				"category_name": "馒头",
+			},
+			{
+				"category_id": 44,
+				"category_name": "面包",
+			}
+		],
+	},
+	{
+		"id": "id9",
+		"name": "饮品",
+		"category": [
+			{
+				"category_id": 45,
+				"category_name": "芬达",
+			},
+			{
+				"category_id": 46,
+				"category_name": "可乐",
+			},
+			{
+				"category_id": 47,
+				"category_name": "雪碧",
+			}
+		],
+	},
+	{
+		"id": "id10",
+		"name": "汤羹",
+		"category": [
+			{
+				"category_id": 48,
+				"category_name": "冬瓜排骨汤",
+			},
+			{
+				"category_id": 49,
+				"category_name": "萝卜骨头汤",
+			},
+			{
+				"category_id": 50,
+				"category_name": "香菇老鸭汤",
+			}
+		],
+	},
+]
+
+module.exports = constants
