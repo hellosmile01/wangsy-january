a75da11ddbfe040d630d82ea6e63d3e97c43fb49
smilewangsy <smilewangsy@outlook.com>
2018-09-11 18:39:07 +0800

    no message

--- january-auth/src/main/java/smile/wangsy/january/auth/JanuaryAuthApplication.java
+++ january-auth/src/main/java/smile/wangsy/january/auth/JanuaryAuthApplication.java
@@ -3,6 +3,10 @@ package smile.wangsy.january.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://www.jianshu.com/p/d94bb118aa43
+ * https://github.com/eugenp/tutorials/tree/master/spring-security-sso
+ */
 @SpringBootApplication
 public class JanuaryAuthApplication {
 
--- january-auth/src/main/java/smile/wangsy/january/auth/config/Oauth2Config.java
+++ january-auth/src/main/java/smile/wangsy/january/auth/config/Oauth2Config.java
@@ -1,13 +1,13 @@
 package smile.wangsy.january.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * @author Juzi
@@ -17,13 +17,27 @@ import javax.sql.DataSource;
 @EnableAuthorizationServer
 public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
     private AuthenticationManager authenticationManager;
 
-    private WebResponseExceptionTranslator webResponseExceptionTranslator;
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 
-    private DataSource dataSource;
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("SampleClientId")
+                .secret("secret")
+                .authorizedGrantTypes("authorization_code")
+                .scopes("user_info")
+                .autoApprove(true);
+    }
 
-    private RedisConnectionFactory redisConnectionFactory;
-
-
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
 }
